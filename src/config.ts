3e82713a11e97a3234d105f3e42f1ad4bd404e4a
ibucoin <ibucoin@gmail.com>
2024-07-26 13:08:41 +0800

    add aboutme

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+export const SITE_TITLE = 'ibucoin(伊布币)'
+
+export const GITHUB = 'https://github.com/ibucoin'
+export const X = 'https://x.com/ibucoin'
+
+export const MENUS = [
+  {
+    title: '首页',
+    link: '/',
+    target: '_self',
+  },
+  {
+    title: '文章',
+    link: '/posts',
+    target: '_self',
+  },
+  {
+    title: '标签',
+    link: '/tags',
+    target: '_self',
+  },
+  {
+    title: '归档',
+    link: '/date',
+    target: '_self',
+  },
+  {
+    title: 'GitHub',
+    link: 'https://github.com/ibucoin',
+    target: '_blank',
+  },
+  {
+    title: 'X',
+    link: 'https://x.com/ibucoin',
+    target: '_blank',
+  },
+]

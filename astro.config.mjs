4df2b0e1acb9fcd82563fd0fa33b9292b81d47ea
ibucoin <ibucoin@gmail.com>
2024-07-27 09:15:49 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,24 @@ import mdx from '@astrojs/mdx'
 import rehypePrettyCode from 'rehype-pretty-code'
 import moonlightTheme from './src/assets/moonlight-ii.json'
 
-export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-    }),
-    mdx(),
-  ],
-  markdown: {
-    syntaxHighlight: false,
-    rehypePlugins: [
-      [
-        rehypePrettyCode,
-        {
-          theme: moonlightTheme,
-        },
-      ],
+export default defineConfig(
+  {
+    integrations: [
+      UnoCSS({
+        injectReset: true,
+      }),
+      mdx(),
     ],
+    markdown: {
+      syntaxHighlight: false,
+      rehypePlugins: [
+        [
+          rehypePrettyCode,
+          {
+            theme: moonlightTheme,
+          },
+        ],
+      ],
+    },
   },
-})
+)

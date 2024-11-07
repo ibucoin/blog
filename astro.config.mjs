03f77c43c1d1fc89c20bafa960c2fea8ea5d2fd8
ibucoin <ibucoin@gmail.com>
2024-11-07 12:28:44 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import mdx from '@astrojs/mdx'
 import rehypePrettyCode from 'rehype-pretty-code'
-import moonlightTheme from './src/assets/moonlight-ii.json'
+import moonlightTheme from './public/theme/moonlight-ii.json'
 
 export default defineConfig(
   {
@@ -14,11 +14,27 @@ export default defineConfig(
     ],
     markdown: {
       syntaxHighlight: false,
+      extendDefaultPlugins: true,
       rehypePlugins: [
         [
           rehypePrettyCode,
           {
             theme: moonlightTheme,
+            defaultLang: 'javascript',
+            onVisitLine(node) {
+              // 防止空行折叠
+              if (node.children.length === 0) {
+                node.children = [{ type: 'text', value: ' ' }]
+              }
+            },
+            onVisitHighlightedLine(node) {
+              // 添加类名到高亮的行
+              node.properties.className.push('highlighted')
+            },
+            onVisitHighlightedWord(node) {
+              // 添加类名到高亮的词
+              node.properties.className = ['word']
+            },
           },
         ],
       ],
